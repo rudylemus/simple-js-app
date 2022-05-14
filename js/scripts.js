8a53454230c87fa07ce2703abb15353944a260cd
Rudy Lemus <rudolfolemus@gmail.com>
2022-05-14 11:51:47 -0700

    replaced content with list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'balbasaur', height: 3, type: ['grass','poison']},
+  {name: 'pikachu', height: 2, type: 'electic'},
+  {name: 'snorlax', height: 9, type: 'normal'},
+  {name: 'machoke', height: 6, type: 'fighting'},
+  {name: 'charizard', height: 7, type: ['fire','flying']}
+];
