5cbcbe7a8c394a03a47946cde503e4d684a407a8
Rudy Lemus <rudolfolemus@gmail.com>
2022-05-15 17:35:17 -0700

    added loop of pokemon names and heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 let pokemonList = [
-  {name: 'balbasaur', height: 3, type: ['grass','poison']},
-  {name: 'pikachu', height: 2, type: 'electic'},
-  {name: 'snorlax', height: 9, type: 'normal'},
-  {name: 'machoke', height: 6, type: 'fighting'},
-  {name: 'charizard', height: 7, type: ['fire','flying']}
+  {name: 'bulbasaur', height: 0.7, type: ['grass','poison']},
+  {name: 'pikachu', height: 0.4, type: 'electic'},
+  {name: 'snorlax', height: 2.1, type: 'normal'},
+  {name: 'machoke', height: 1.5, type: 'fighting'},
+  {name: 'charizard', height: 1.7, type: ['fire','flying']}
 ];
+//loop to display all pokemon with their heights
+for (let i = 0; i < pokemonList.length; i++)
+{
+  document.write(pokemonList[i].name + " " + pokemonList[i].height + " ");
+}
+
+  if (pokemonList.height > 2.0) {
+    document.write('Wow that\'s big!');
+  }
