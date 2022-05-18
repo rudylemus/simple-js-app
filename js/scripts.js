686e68bb87d448635241f5e5800e6e554f79267d
Rudy Lemus <rudolfolemus@gmail.com>
2022-05-17 23:50:55 -0700

    placed array in IIFE and added return add and getAll

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
+//placed array in IIFE
+let pokemonRepository = (function() {
 let pokemonList = [
   {name: 'Bulbasaur', height: 0.7, type: ['grass','poison']},
   {name: 'Pikachu', height: 0.4, type: 'electic'},
   {name: 'Snorlax', height: 2.1, type: 'normal'},
   {name: 'Machoke', height: 1.5, type: 'fighting'},
   {name: 'Charizard', height: 1.7, type: ['fire','flying']}
-]
+];
+
+return {
+  add: function(pokemon) {
+    pokemonList.push(pokemon);
+  },
+  getAll: function() {
+    return pokemonList;
+  }
+};
+})();
 
 // internal anonymous function to loop list
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.forEach(function(pokemon) {
     document.write(' ' + pokemon.name + ' height: ' + pokemon.height);
         if (pokemon.height > 2.0) {
           document.write('Wow that\'s big! ');
         }
   });
+
+// document.write(pokemonRepository.getAll());
