69c817dba4afed47534dd8ffe92dfc71ae21a267
Rudy Lemus <rudolfolemus@gmail.com>
2022-05-17 22:44:34 -0700

    deleted for loop and inserted forEach loop
    also capitalized pokemon name for easier reading

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
 let pokemonList = [
-  {name: 'bulbasaur', height: 0.7, type: ['grass','poison']},
-  {name: 'pikachu', height: 0.4, type: 'electic'},
-  {name: 'snorlax', height: 2.1, type: 'normal'},
-  {name: 'machoke', height: 1.5, type: 'fighting'},
-  {name: 'charizard', height: 1.7, type: ['fire','flying']}
-];
-//loop to display all pokemon with their heights
-for (let i = 0; i < pokemonList.length; i++)
-{
-document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + ") ");
+  {name: 'Bulbasaur', height: 0.7, type: ['grass','poison']},
+  {name: 'Pikachu', height: 0.4, type: 'electic'},
+  {name: 'Snorlax', height: 2.1, type: 'normal'},
+  {name: 'Machoke', height: 1.5, type: 'fighting'},
+  {name: 'Charizard', height: 1.7, type: ['fire','flying']}
+]
 
-   if (pokemonList[i].height > 2.0) {
-document.write('Wow that\'s big! ');
-  }
-
-}
+// internal anonymous function to loop list
+pokemonList.forEach(function(pokemon) {
+    document.write(' ' + pokemon.name + ' height: ' + pokemon.height);
+        if (pokemon.height > 2.0) {
+          document.write('Wow that\'s big! ');
+        }
+  });
