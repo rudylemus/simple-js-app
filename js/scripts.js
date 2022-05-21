2cdeba8d02bc0ff2512e0ad68ec2bfd73378002c
Rudy Lemus <rudolfolemus@gmail.com>
2022-05-20 19:05:09 -0700

    added styling to list buttons

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,59 @@ let pokemonList = [
   {name: 'Charizard', height: 1.7, type: ['fire','flying']}
 ];
 
+function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+
+function getAll() {
+  return pokemonList;
+}
+function addListItem(pokemon) {
+  let pokemonList = document.querySelector('.pokemon-list'); // selects list
+  let listItem = document.createElement('li'); // creates li
+  let button = document.createElement('button'); // creates button
+  button.innerText = pokemon.name; // display pokemon name in button
+  button.classList.add('pokemon-list-button'); // adds class name to button for styling
+  listItem.appendChild(button); // appends button to li so that it shows up
+  pokemonList.appendChild(listItem); // appends li to ul
+  addEvent(button, pokemon);
+}
+function addEvent(button,pokemon) {
+  button.addEventListener('click', function() {
+    showDetails(pokemon);
+
+  });
+}
+
+function showDetails(pokemon) {
+  console.log(pokemon);
+
+}
+
 return {
   add: function(pokemon) {
-    pokemonList.push(pokemon);
-  },
-  getAll: function() {
-    return pokemonList;
-  }
-};
+   pokemonList.push(pokemon);
+ },
+ getAll: function() {
+   return pokemonList;
+ },
+  addListItem: addListItem
+}
+
 })();
 
-// internal anonymous function to loop list
+
 pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(' ' + pokemon.name + ' height: ' + pokemon.height);
-        if (pokemon.height > 2.0) {
-          document.write('Wow that\'s big! ');
-        }
-  });
+  pokemonRepository.addListItem(pokemon);
+});
+
+console.log(pokemonRepository.getAll());
